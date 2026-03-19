965213d78e9ca5fd738f8506b9b0a7b7eeb36885
victor-hugo-ss <victorabreu2512@gmail.com>
2026-03-19 17:13:37 -0300

    feat: user update functionality.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -20,6 +20,27 @@ module.exports = class UserController {
     res.redirect("/users");
   }
 
+  static async updateUser(req, res) {
+    const id = req.params.id;
+    const user = await User.findOne({ raw: true, where: { id: id } });
+    res.render("users/edit", { user });
+  }
+
+  static async updateUserPost(req, res) {
+    const id = req.body.id;
+
+    const user = {
+      name: req.body.name,
+      lastname: req.body.lastname,
+      occupation: req.body.occupation,
+      age: req.body.age,
+    };
+
+    await User.update(user, { where: { id: id } });
+    console.log("User updated successfully!");
+    res.redirect("/users");
+  }
+
   static async viewDetailsUser(req, res) {
     const id = req.params.id;
     const user = await User.findOne({ raw: true, where: { id: id } });
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -5,6 +5,8 @@ const UserController = require("../controllers/UserController");
 
 router.get("/add", UserController.createUser);
 router.post("/add", UserController.createUserSave);
+router.get("/edit/:id", UserController.updateUser);
+router.post("/edit", UserController.updateUserPost);
 router.get("/user/:id", UserController.viewDetailsUser);
 router.get("/", UserController.homeUser);
 router.get("/users", UserController.showUser);
