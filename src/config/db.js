52e4de76e60e07f007df5c56094756b511a29c8b
victor-hugo-ss <victorabreu2512@gmail.com>
2026-03-19 11:58:45 -0300

    feat: configuring the database connection with Sequelize

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+);
+
+try {
+  sequelize.authenticate();
+  console.log("Connected with MySQL!");
+} catch (error) {
+  console.log(error);
+}
+
+module.exports = sequelize;
